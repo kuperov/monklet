224e90ae0062896cccb72a5ebe28aabca0faf4d9
Alex Cooper <alex@acooper.org>
2024-09-16 10:20:32 +1000

    what a stupid hacky bug

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -370,6 +370,7 @@ def project_simulate(request: HttpRequest, pk: str) -> HttpResponse:
     project = get_object_or_404(Project, pk=pk)
     if not project.can_view(request.user):
         raise PermissionDenied("User action not permitted.")
+    interview = None
     if request.GET.get('bot'):
         bot = get_object_or_404(Bot, pk=request.GET['bot'])
         if bot.project.pk != project.pk:
@@ -387,8 +388,9 @@ def project_simulate(request: HttpRequest, pk: str) -> HttpResponse:
         "bots": project.enabled_bots(),
         "test_interviews": project.test_interviews(),
         "menu_data": menu(project),
-        "initial_interview": interview.pk
     })
+    if interview:
+        ctx["initial_interview"] = interview.pk
     return render(request, "bots/simulator.html", ctx)
 
 @login_required
