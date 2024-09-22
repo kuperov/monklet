96afd2961afbf1549f4ce31ea928876cb5e752ca
Alex Cooper <alex@acooper.org>
2024-09-23 05:12:01 +1000

    add cache buster

--- config/context_processors.py
+++ config/context_processors.py
@@ -1,15 +1,3 @@
-# def language_code(request):
-#     return {"LANGUAGE_CODE": request.LANGUAGE_CODE}
-
-
-# def get_cookie(request):
-#     return {"COOKIES": request.COOKIES}
-
-
-# # Add the 'ENVIRONMENT' setting to the template context
-# def environment(request):
-#     return {"ENVIRONMENT": settings.ENVIRONMENT}
-
 
 def _random_string(length):
     import random
@@ -21,9 +9,7 @@ def _random_string(length):
     random_string = "".join(random.choice(characters) for _ in range(length))
     return random_string
 
-
 CACHE_BUSTER = _random_string(length=10)
-
 
 def cache_buster(_request):
     return dict(CACHE_BUSTER=CACHE_BUSTER)
