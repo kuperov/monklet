960acba277a1e403e9bde6f541fc4d93b4571cf3
Alex Cooper <alex@acooper.org>
2024-09-24 22:54:57 +1000

    basic tests of interview landing pages etc

--- apps/projects/test_interviews.py
+++ apps/projects/test_interviews.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse_lazy
+from django.core import mail
+
 from apps.projects import models
 from apps.users.models import User
 
@@ -9,9 +11,9 @@ _e, _pw = "a@b.com", "tiger"
 class InterviewTest(TestCase):
 
     def setUp(self):
-        self.owner = User.objects.create(email=_e, password=_pw)
+        self.owner = User.objects.create_user(email=_e, password=_pw, name='Max Dingus')
         self.project = models.Project.objects.create(owner=self.owner, name="XYZ")
-        self.bot_nc = models.Bot.objects.create(project=self.project, name='B', allow_public=True)
+        self.bot_nc = models.Bot.objects.create(project=self.project, name='BBB', status='live', allow_public=True)
         self.letter = models.ConsentLetter.objects.create(
             project=self.project,
             name=".",
@@ -83,3 +85,26 @@ class InterviewTest(TestCase):
         resp = self.client.get(let_url)
         for t in ['<h1>PQR</h1>', '<p>pqrstuv <strong>wx</strong> yz']:
             self.assertContains(resp, t)
+
+    def test_invite(self):
+        self.assertTrue(self.client.login(email=_e, password=_pw))
+        resp = self.client.get(self.project.bots_url)
+        self.assertContains(resp, 'BBB', status_code=200)
+        public_page = reverse_lazy("bot-public", kwargs={"pk": self.bot_let.pk})
+        self.assertContains(resp, public_page, status_code=200)  # as menu item link
+        project_inv = reverse_lazy('project-interviews-invite', kwargs={'pk': self.project.pk})
+        bot_invite = f"{project_inv}?bot={self.bot_nc.pk}"
+        self.assertContains(resp, bot_invite, status_code=200)  # will fail
+        resp = self.client.get(bot_invite)
+        # correct bot should be selected
+        self.assertContains(resp, f'<option value="{self.bot_nc.pk}" selected>', status_code=200)
+        # create invitation
+        mail.outbox.clear()
+        resp = self.client.post(project_inv, {'bot': self.bot_nc.pk, 'subject_name': 'Sally', 'subject_email': 'saz@hotmail.com', 'Save': 'save'})
+        iv = models.Interview.objects.get(subject_email='saz@hotmail.com')
+        iv_url = reverse_lazy('interview-landing', kwargs={'pk': iv.pk})
+        self.assertEqual(iv.bot.pk, self.bot_nc.pk)
+        self.assertEqual(iv.subject_name, 'Sally')
+        msg = mail.outbox.pop()
+        self.assertEqual(msg.to, ["Sally <saz@hotmail.com>"])
+        self.assertIn(str(iv_url), msg.body)
