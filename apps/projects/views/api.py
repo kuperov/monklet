43827522226c7d5d21840b774d49e5f1d63ce98e
Alex Cooper <alex@acooper.org>
2024-11-19 23:01:46 +1100

    improve example api call

--- apps/projects/views/api.py
+++ apps/projects/views/api.py
@@ -37,6 +37,7 @@ def api_access_example(request: HttpRequest, pk: str) -> HttpResponse:
     api_url = request.build_absolute_uri(
         reverse("api-all-cases", kwargs={"pk": project.pk})
     )
+    api_url = api_url.replace(pk, "{MONKLET_PROJECT}")
     return render(
         request, "api/_python_example.html", {"project": project, "api_url": api_url}
     )
